f7b33d452116d821a43ee67aea9a5e5bb5875d42
shinshin08 <kosum1n0828@gmail.com>
2026-06-29 22:35:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const messages = [
+  "今日は5問だけ遊ぶニャ♪",
+  "復習するともっと強くなれるニャ！",
+  "かのん、おかえり🌸",
+  "一緒に頑張ろうニャ！",
+  "疲れたら少し休憩ニャ☕"
+];
 
+const cat = document.getElementById("cat");
+const message = document.getElementById("message");
+
+cat.addEventListener("click", () => {
+
+    cat.style.transform = "scale(1.15)";
+
+    setTimeout(()=>{
+        cat.style.transform = "scale(1)";
+    },200);
+
+    const random = Math.floor(Math.random()*messages.length);
+
+    message.textContent = messages[random];
+
+});
